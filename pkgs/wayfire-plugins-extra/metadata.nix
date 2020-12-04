0af5efb5b0807b252a0e2d76e56cf4263d60a779
ishan <ishan365.ia@gmail.com>
2020-12-04 20:11:12 +0530

    update the sha256

--- pkgs/wayfire-plugins-extra/metadata.nix
+++ pkgs/wayfire-plugins-extra/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://github.com/WayfireWM/wayfire-plugins-extra";
   branch = "master";
   rev = "04ea0b23fd67b7dcfce3c6a19f0d1ab34c01ef5b";
-  sha256 = "0000000000000000000000000000000000000000000000000000";
+  sha256 = "sha256-YtTRnI2Z1Xr+gk9zgSGlA34gyw+hEio9M2f0cnlRB4o=";
 }
